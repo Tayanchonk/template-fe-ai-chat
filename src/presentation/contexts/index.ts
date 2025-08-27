3fe0f74f5664ab7892a67c56cc804b5ad9e35f71
tayankla <tayankla@metrosystems.co.th>
2025-08-27 21:45:28 +0700

    Update index.ts

--- src/presentation/contexts/index.ts
+++ src/presentation/contexts/index.ts
@@ -1,4 +1,11 @@
-export { AuthProvider } from './AuthContext';
-export { ChatProvider } from './ChatContext';
+// Export context providers
+export { AuthProvider } from './AuthContext.tsx';
+export { ChatProvider } from './ChatContext.tsx';
+
+// Export custom hooks
 export { useAuthContext } from './useAuthContext';
 export { useChatContext } from './useChatContext';
+
+// Export context objects
+export { AuthContext } from './authContext';
+export { ChatContext } from './chatContext';
